2f0ee9886aca2de9094a3ca75d204e100bbb177a
Felipe Bellini <felipe.bellini@krykto.com>
2024-04-06 16:49:28 -0300

    initial page(not working...)

--- src/Presentation/Screens/LibraryScreen.java
+++ src/Presentation/Screens/LibraryScreen.java
@@ -3,22 +3,27 @@ package Presentation.Screens;
 import core.use_cases.BookUseCase;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
-public class LibraryScreen {
+public class LibraryScreen extends JFrame {
     private JPanel panel1;
     private JTable table1;
     private JTextField textField1;
     private JTextField textField2;
-    private JComboBox comboBox1;
     private JTextField textField3;
     private JButton pesquisarButton;
     private BookUseCase bookUseCase;
 
     public LibraryScreen(){
-        getBooks();
+        bookUseCase = new BookUseCase();
+        setWindow();
     }
 
-    private void getBooks(){
-//        bookUseCase.
+    private void setWindow(){
+        setTitle("Gerenciador da Biblioteca");
+        setSize(1200, 780);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
